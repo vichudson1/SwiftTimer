cf9ba2a9efa4a6f26901b1428b44d6d018b3e5fe
Victor Hudson <vichudson1@gmail.com>
2016-04-20 09:09:28 -0400

    Documented The Class & Other Improvements. * Added a parameter to the firing action closure so the timer instance gets passed into it. * Fixed an oversight where I'd forgotten to make the userInfo dictionary useful, accessible in the firing action closure via timer.userInfo * Once again tried to fix the code formatting in the Read Me.

--- SwiftTimer/SwiftTimer.swift
+++ SwiftTimer/SwiftTimer.swift
@@ -7,19 +7,16 @@
 
 import Foundation
 
+/// A simple Swift wrapper for NSTimer. Use it just as you would NSTimer with the exectpion that your provide a closure to perform when the timer fires instead of a method selector.
 public class SwiftTimer: NSObject {
-	func fire() {
-		self.timer?.fire()
-		running = true
-	}
-	
-	func invalidate() {
-		self.timer?.invalidate()
-		self.timer = nil
-		running = false
-	}
-	
-	init(timeInterval: NSTimeInterval, userInfo: [String: AnyObject]?, repeats: Bool, timerAction: () -> Void){
+	/**
+	Use this `init` to update your views progress.
+	- parameter timeInterval: The delay interval between firing events of the timer.
+	- parameter userInfo: A dictionary you can pass along with the timer for use during firing events.
+	- parameter repeats: Determines whether the repeats or fires a single time.
+	- parameter timerAction: A closure that will be executed each firing of the timer. The closure has a parameter which passes in the instance of the timer executing it.
+	*/
+	init(timeInterval: NSTimeInterval, userInfo: [String: AnyObject]?, repeats: Bool, timerAction: (_: SwiftTimer) -> Void){
 		super.init()
 		self.timerAction = timerAction
 		self.timer = NSTimer.scheduledTimerWithTimeInterval(timeInterval,
@@ -29,15 +26,32 @@ public class SwiftTimer: NSObject {
 		                                                    repeats: repeats)
 	}
 	
+	/// Use this method to start your timer after creating an instance.
+	func fire() {
+		self.timer?.fire()
+		running = true
+	}
+	
+	/// Use this method to cancel your timer when you are done with it.
+	func invalidate() {
+		self.timer?.invalidate()
+		self.timer = nil
+		running = false
+	}
+	
+	/// User Info Dictionary For information you'd like assigned to this timer.
+	var userInfo: [String: AnyObject]?
+	
+	/// Check to see if the timer is running or not.
 	var running = false
 	
 	private var timer: NSTimer?
-	private var timerAction: (() -> Void)?
+	private var timerAction: ((_: SwiftTimer) -> Void)?
 }
 
 private extension SwiftTimer {
 	@objc func theSelector(timer: NSTimer) {
 		guard let action = self.timerAction else { return }
-		action()
+		action(self)
 	}
 }
